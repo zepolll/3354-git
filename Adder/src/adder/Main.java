c178091520ede502d2d6cf632507c2562b9e1b07
Raul Lopez <onlinezepol@gmail.com>
2020-09-24 18:33:53 -0500

    Modified Main.java
    Changed the code to add an arbitary amount of positive integers.
    Modified error message.

---
+++ Adder/src/adder/Main.java
@@ -0,0 +1,24 @@
+package adder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            int result = addArguments(args);
+            System.out.println(result);
+        } catch (Exception e) {
+            if(args.length<1)
+		    System.out.println("Please enter any amount of positive integers.");
+	    else 
+		    System.out.println("Error in input"); 
+        }
+    }
+
+    private static int addArguments(String[] args) {
+	int result = 0;
+	for(int i =0;i<args.length;i++){
+		result += Integer.valueOf(args[i]);	
+	}	
+        return result;
+    }
+}
